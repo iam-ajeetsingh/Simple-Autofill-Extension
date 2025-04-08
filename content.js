7d702f534c63b50c0ea0c0cf242c22c4774851e6
Ajeet Singh <ajeet@arizona.edu>
2025-04-07 20:00:02 -0700

    added the code for auto-fill action and options.html file

--- content.js
+++ content.js
@@ -13,30 +13,44 @@ function autofill() {
   document.hasAutofilled = true;
 
   // --- Autofill Configuration ---
-  const usernameValue = "";
-  const password = ""
-  const commentValue = "";
+  const usernameValue = "Ajeet_Singh";
+  const password = "*********"
+  const commentValue = "This is a test comment.";
 
   // --- Autofill Logic ---
-  const usernameInput = document.querySelector();
+  const usernameInput = document.querySelector("input[name='username']"); //Selector for username field
   if (usernameInput) {
     // Fill the value of the input here
+    usernameInput.value = usernameValue;
+    // Trigger input event to notify the browser of the change
+    const event = new Event('input', { bubbles: true });
+    usernameInput.dispatchEvent(event);
+
     console.log(`Filled user name with: ${usernameValue}`);
   } else {
     console.log(`User name field not found.`);
   }
 
-  const passwordInput = document.querySelector();
+  const passwordInput = document.querySelector("input[name='password']"); //Selector for password field
   if (passwordInput) {
     // Fill the value of the input here
+    passwordInput.value = password;
+    // Trigger input event to notify the browser of the change
+    const event = new Event('input', { bubbles: true });
+    passwordInput.dispatchEvent(event);
     console.log(`Filled password with: ${password}`);
   } else {
     console.log(`Password field not found.`);
   }
 
-  const commentsInput = document.querySelector();
+  const commentsInput = document.querySelector("textarea[name='comments']"); //Selector for comments field
   if (commentsInput) {
     // Fill the value of the input here
+    commentsInput.value = commentValue;
+    // Trigger input event to notify the browser of the change
+    const event = new Event('input', { bubbles: true });
+    commentsInput.dispatchEvent(event);
+
     console.log(`Filled comment with: ${commentValue}`);
   } else {
     console.log(`Comment field not found.`);
